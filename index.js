d682e8c7ff794ecdb7920639fce5d2a12c1d2fec
ShamimHossain1 <beastb697@gmail.com>
2023-05-28 16:29:14 +0600

    'mongodb'

--- index.js
+++ index.js
@@ -12,7 +12,23 @@ app.use(express.json());
 
 
 
+const uri = "mongodb+srv://toys:CISXvktUyAagLWCz@cluster0.n1ihip4.mongodb.net/?retryWrites=true&w=majority";
 
+/// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+  
+    
+  
+  
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      //await client.close();
+    }
+  }
+  run().catch(console.dir);
   
   
   
