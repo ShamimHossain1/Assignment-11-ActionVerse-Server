6a2f779a7daf0e54d61bf72b9dc3428d2128b13a
ShamimHossain1 <beastb697@gmail.com>
2023-05-28 16:33:17 +0600

    'get-alltoysId'

--- index.js
+++ index.js
@@ -55,7 +55,24 @@ const client = new MongoClient(uri, {
         const result = await cursor.toArray();
         res.send(result);
       })
-      
+      app.get('/allToys/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await toysCollection.findOne(query)
+        res.send(result);
+      })
+  
+      // my toys
+     
+      try {
+        const result = await toysCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error searching for documents:', error);
+        res.status(500).send('Internal Server Error'); 
+      }
+    });
+  
     //api http://localhost:5000/search?query=
   
     
